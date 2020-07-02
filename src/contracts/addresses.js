5ee01fd1a3b93ff155a09c8e782e039f0ea0d745
JoeFilmo <joefilmo@protonmail.com>
2020-07-01 23:02:02 -0700

    Update addresses

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -1,8 +1,9 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  askoToken: "0x0",
-  askoPresale: "0x0"
+  askoToken: "0xeEEE2a622330E6d2036691e983DEe87330588603",
+  askoStaking: "0xE346A9Fa3414645D4Ac383d47705B8f663A37B58",
+  askoPresale: "0xa1021b1875944c76977898397A03D3b0f176f27C"
 };
 
 export default addresses;
